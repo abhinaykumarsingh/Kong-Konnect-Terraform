24ec22ac924651fa42e19595afdcb0d6a0d6f3b5
Abhinay Singh <abhinaykumarsingh28@gmail.com>
2026-04-10 00:34:11 +0530

    push again

--- consumer.tf
+++ consumer.tf
@@ -5,7 +5,7 @@ resource "konnect_gateway_consumer" "user1" {
 }
 
 resource "konnect_gateway_key_auth" "my_keyauth" {
-  key              = "spiderxkey"
+  key              = "spiderx"
   consumer_id      = konnect_gateway_consumer.user1.id
   control_plane_id = var.control_plane_id
 }
