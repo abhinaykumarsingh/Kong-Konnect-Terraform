d46effb6186f574da8050bf644f1563c3bee75c8
Abhinay Singh <abhinaykumarsingh28@gmail.com>
2026-04-10 13:24:57 +0530

    securing api key in environment variable

--- consumer.tf
+++ consumer.tf
@@ -5,7 +5,7 @@ resource "konnect_gateway_consumer" "user1" {
 }
 
 resource "konnect_gateway_key_auth" "my_keyauth" {
-  key              = "spiderx"
+  key              = var.api_key_value
   consumer_id      = konnect_gateway_consumer.user1.id
   control_plane_id = var.control_plane_id
 }
